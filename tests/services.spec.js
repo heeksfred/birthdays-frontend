ab49d84b7b0ad98c2104d0bdc36180bf76152318
Fred Heeks <heeksfred@gmail.com>
2019-11-30 23:21:36 +0000

    Further tests and function to calculate a persons age

--- tests/services.spec.js
+++ tests/services.spec.js
@@ -1,5 +1,19 @@
 const serv = require("../src/services");
 
+var birthday_list = [];
+
+beforeAll(() => {
+  // Clears the birthday_list and adds some test data.
+  birthday_list.length = 0;
+  let now = new Date();
+  birthday_list.push({name: "John Doe", birthday: "1981-08-02"});
+  birthday_list.push({name: "Katie Smith", birthday: "1973-05-20"});
+  birthday_list.push({name: "Anna Jackson", birthday: "1993-10-15"});
+  birthday_list.push({name: "Fred Heeks", birthday: "1970-" + now[1] + "-" + now[2]});
+  let nextdate = new Date("1970",now.getMonth(),now.getDate()+14);
+  birthday_list.push({name: "Next Date", birthday: "1970-" + (nextdate.getMonth() + 1) + "-" + nextdate.getDate()});
+});
+
 describe("Check date range", () => {
   test("it should return all birthdays within a date range", () => {
 
@@ -27,3 +41,12 @@ describe("Check birthdays in the next two weeks", () => {
       expect(serv.getBirthdaysInNextTwoWeeks()).toEqual([{birthday: nowdate, name: "Fred Heeks"},{birthday: fortnight , name: "Next Date"}]);
     });
 });
+
+describe("Calculate ages of people in the birthday list", () => {
+  test("it should return the persons age who has a birthday today (50)", () => {
+
+      const birthdayBoy = serv.getTodaysBirthdays();
+      console.log("Birthday boy is " + birthdayBoy[0].name);
+      expect(serv.calculateAge(birthdayBoy[0].birthday)).toEqual(49);
+  })
+})
